8dcb0578f4095a2af1f68d5bcccfae3da93d5caf
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-08-12 17:54:13 +0200

    feat(db): set up SQLite proxy with Drizzle ORM integration

---
+++ src/lib/db/index.ts
@@ -0,0 +1,38 @@
+import { drizzle } from 'drizzle-orm/sqlite-proxy';
+import { schema } from '$lib/db/schema';
+import Database from '@tauri-apps/plugin-sql';
+
+/**
+ * Exports the SQLite database instance as a promise.
+ */
+export const sqlite: Database = await Database.load(
+	`sqlite:${import.meta.env.DEV ? 'DEV-' : ''}db.sqlite`
+);
+
+export const db = drizzle<typeof schema>(
+	async (sql, params, method) => {
+		let results: any = []; // eslint-disable-line
+
+		const isSelect = /^\s*SELECT\b/i.test(sql); // Prüft, ob es ein SELECT-Befehl ist
+
+		// Wenn die Abfrage ein SELECT ist, nutze die select-Methode
+		if (isSelect) {
+			results = await sqlite.select(sql, params).catch(() => {
+				return [];
+			});
+		} else {
+			// Ansonsten, nutze die execute-Methode
+			await sqlite.execute(sql, params).catch(() => {
+				return [];
+			});
+			return { rows: [] };
+		}
+
+		// Falls notwendig, mappe die Ergebnisse auf Arrays
+		results = results.map((row: any) => Object.values(row)); // eslint-disable-line
+
+		// Wenn die Methode "all" ist, gebe alle Zeilen zurück
+		return { rows: method === 'all' ? results : results[0] };
+	},
+	{ schema: schema, logger: import.meta.env.DEV }
+);

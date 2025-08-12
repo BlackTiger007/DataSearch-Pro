045da555cd0d5e1a5ff25ccc3964408f2fd3a05b
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-08-12 17:55:44 +0200

    feat(tauri): add automatic migration loading and apply on startup

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,23 @@
+mod migrations;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    // Migrations automatisch laden
+    let migrations = migrations::load_migrations();
+
+    // Abhängig vom Build-Typ den DB-Pfad setzen
+    let db_path = if cfg!(debug_assertions) {
+        "sqlite:DEV-db.sqlite"
+    } else {
+        "sqlite:db.sqlite"
+    };
+
     tauri::Builder::default()
-        .plugin(tauri_plugin_sql::Builder::new().build())
+        .plugin(
+            tauri_plugin_sql::Builder::new()
+                .add_migrations(db_path, migrations)
+                .build(),
+        )
         .plugin(tauri_plugin_opener::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");

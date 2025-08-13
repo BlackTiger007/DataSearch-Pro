f71fe500077e6e3dc890696915ee048ba95a0298
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-08-13 19:47:23 +0200

    feat(ui): add navigation bar with links to main pages

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,6 @@
+<div class="navbar bg-base-100 shadow-sm">
+	<a href="/" class="btn text-xl btn-ghost">DataSearch-Pro</a>
+	<a href="/dashboard" class="btn text-xl btn-ghost">Dashboard</a>
+	<a href="/settings" class="btn text-xl btn-ghost">Einstellungen</a>
+	<a href="/about" class="btn text-xl btn-ghost">About</a>
+</div>

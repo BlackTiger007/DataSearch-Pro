114fa61da53d3b493c11dc28219eed7962e0adf0
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-08-18 19:07:42 +0200

    fix(sentry): add environment configuration to Sentry initialization

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -4,6 +4,8 @@ import * as Sentry from '@sentry/sveltekit';
 Sentry.init({
 	dsn: 'https://4e23fc19f0b340f5bd761a87eb9892f1@glitchtip.webretter.com/2',
 
+	environment: import.meta.env.MODE,
+
 	tracesSampleRate: 1.0,
 
 	// Enable logs to be sent to Sentry

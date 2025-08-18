116a63b3af75a59db2fa0aac2a3dfb6b88b9c057
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-08-18 18:20:52 +0200

    refactor(layout): remove favicon import and head element for cleaner layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,10 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
 	import Navbar from '$lib/components/navbar.svelte';
 	import { settings } from '$lib/stores/settings.svelte';
 
 	let { children } = $props();
 </script>
-
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
 
 <div class="flex min-h-screen flex-col bg-base-300" data-theme={settings.theme}>
 	<Navbar />
